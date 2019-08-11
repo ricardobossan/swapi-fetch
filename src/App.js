5f7ec4e20c0f7bd0db0c201a574d6067f869b9c7
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2019-08-10 23:33:48 -0300

    make request to SWAPI pass the cors requiremente

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
+import swapi from 'swapi-node';
+import styled from 'styled-components';
+import axios from 'axios';
+
 import './App.css';
+
+const proxyurl = 'https://cors-anywhere.herokuapp.com/';
+const url = 'https://swapi.co/api'; // site that doesn’t send Access-Control-*
+fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
+  .then(response => response.text())
+  .then(contents => console.log(contents))
+  .catch(() =>
+    console.log('Can’t access ' + url + ' response. Blocked by browser?')
+  );
+/* axios
+  .get('https://swapi.co/api', {
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  })
+  .then(res => console.log(res.status));
+ */
+/* fetch('https://swapi.co/api', {
+  method: 'GET',
+  mode: 'cors',
+  headers: { 'Access-Control-Allow-Origin': '*' }
+}).then(result => console.log(result));
+ */
+const Title = styled.h1`
+  color: yellow;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <Title>Star Wars Planets</Title>
+        {/*       <section>
+        <ol>
+          <li>POPULATION:{planet.population}</li>
+          <li>CLIMATE: {planet.climate}</li>
+          <li>TERRAIN: {planet.desert}</li>
+        </ol>
+        <div>Featured in {planet.nFilms} films</div>
+      </section>
+ */}{' '}
+      </main>
     </div>
   );
 }
