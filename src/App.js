b5a50205f3dfa5e63a92c02029f43c1edaff5ae0
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2019-08-13 03:06:41 -0300

    inserir dados corretos nos respectivos campos.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 import './App.css';
-
-let planet = '';
 
 // credit: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/random#Gerando_um_n%C3%BAmero_inteiro_aleat%C3%B3rio_entre_dois_valores_inclusive
 let number = function getRandomIntInclusive(min, max) {
@@ -12,27 +10,44 @@ let number = function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-fetch(`https://swapi.co/api/planets/${number(1, 61)}/`)
-  .then(res => res.json())
-  .then(result => localStorage.setItem('planet', JSON.stringify(result)))
-  .then(console.log(localStorage.getItem('planet')));
-
 const Title = styled.h1`
   color: yellow;
 `;
 
 function App() {
+  const [planet, setPlanet] = useState({});
+
+  useEffect(() => {
+    fetch(`https://swapi.co/api/planets/${number(1, 61)}/`)
+      .then(res => res.json())
+      .then(function(result) {
+        return JSON.stringify(result);
+      })
+      .then(function(result) {
+        console.log(result);
+        return JSON.parse(result);
+      })
+      .then(rs => {
+        console.log(rs);
+        setPlanet(rs);
+      });
+  }, []);
   return (
     <div className="App">
       <main>
         <Title>Star Wars Planets</Title>
         <section>
+          <h1>{planet.name}</h1>
           <ol>
             <li>POPULATION:{planet.population}</li>
             <li>CLIMATE: {planet.climate}</li>
-            <li>TERRAIN: {planet.desert}</li>
+            <li>TERRAIN: {planet.terrain}</li>
           </ol>
-          <div>Featured in {planet.nFilms} films</div>
+          <div>
+            Featured in{' '}
+            {typeof planet.films === 'object' ? planet.films.length : null}{' '}
+            films
+          </div>
         </section>
       </main>
     </div>
