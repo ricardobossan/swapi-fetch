4df40bd5b24686e8e0c4119f556130ffdf5f9d2e
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2019-08-17 22:33:11 -0300

    Updates

---
+++ precache-manifest.e511c828c1cd0be758772fb41aa13c51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db2bf6a96a4b4cd2905ebe3e6e12ff4e",
+    "url": "/swapi-fetch/index.html"
+  },
+  {
+    "revision": "90da7bf2c815c6787c4c",
+    "url": "/swapi-fetch/static/css/main.40bacccb.chunk.css"
+  },
+  {
+    "revision": "69c7669cd297861b4443",
+    "url": "/swapi-fetch/static/js/2.9e5054dd.chunk.js"
+  },
+  {
+    "revision": "90da7bf2c815c6787c4c",
+    "url": "/swapi-fetch/static/js/main.578fcf2f.chunk.js"
+  },
+  {
+    "revision": "8a6aef6b0fb52175ec20",
+    "url": "/swapi-fetch/static/js/runtime~main.45fd9a6f.js"
+  }
+]);
